25a9143ca634313798996c04e3b603fc6dcd4eb5
Wilson Wang <wilsonw926@berkeley.edu>
2020-04-15 20:36:05 -0700

    forgot to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_033338) do
+ActiveRecord::Schema.define(version: 2020_04_16_033514) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
