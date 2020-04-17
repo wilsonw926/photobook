7118ab3317b1b32be0645aa4408befb4964caf52
Wilson Wang <wilsonw926@berkeley.edu>
2020-04-17 12:20:48 -0700

    revert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = Rails.application.credentials.dig(:CLOUDFRONT_ENDPOINT)
+  config.action_controller.asset_host = ENV['CLOUDFRONT_ENDPOINT']
   config.serve_static_files = true
   config.assets.digest = true
   config.assets.enabled = true
